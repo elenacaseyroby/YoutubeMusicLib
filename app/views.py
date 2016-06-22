765e72e20d4a8b4088af3e839a14c45cbc8b7da7
elenacaseyroby <elenacaseyroby@gmail.com>
2016-06-22 12:42:58 -0400

    added logic to route to and use logins form

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
+#use namespaces to pull controllers and models
 
 @app.route('/')
 @app.route('/index')
@@ -13,6 +16,21 @@ def index():
 
 def cleandata():
 	return render_template('cleandata.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
+
 
 		
 
