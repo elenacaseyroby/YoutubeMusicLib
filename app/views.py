c5f106d62bd60f3f095a8afb0b374453cb1d54fb
elenacaseyroby <elenacaseyroby@gmail.com>
2016-07-12 00:16:36 -0400

    started a listens page. So far it pulls one row of data from the user’s listens table.

--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@ from flask import render_template, flash, redirect, request, Flask
 from app import app, models, session #,db
 from .forms import LoginForm
 from json import loads
+from sqlalchemy import text
 
-
+user_id = 1;
 
 @app.route('/')
 @app.route('/index')
@@ -54,9 +55,110 @@ def postlistens():
     return "success"
     
 @app.route('/listens')
+def listens():
+  listens_vid_data = getlistensdata() #should be able to access in template now
+  print "listens vid data"
+  print listens_vid_data
+  return render_template('listens.html', listens_vid_data = listens_vid_data)
 
-def cleandata():
-	return render_template('cleandata.html')
+@app.route('/getlistensdata')
+def getlistensdata():
+  limit = 3
+  saved_vids = session.query(models.SavedVid).filter_by(user_id = user_id).first()
+  if not saved_vids:
+    sql = text("""SELECT listens.youtube_id
+   , listens.time_of_listen
+   , videos.youtube_title
+   , videos.title
+   , videos.music
+   , videos.release_date
+   , artists.artist_name as artist
+   , cities.name
+   , albums.name
+   FROM listens
+   JOIN videos ON listens.youtube_id = videos.youtube_id
+   JOIN albums ON videos.album_id = albums.id
+   JOIN artists ON videos.artist_id = artists.id
+   JOIN cities ON artists.city_id = cities.id
+   WHERE listens.user_id = """+str(user_id)+"""
+   AND listens.time_of_listen > '2016-07-05 19:12:18'
+   AND listens.time_of_listen < '2016-07-10 19:12:18'
+   AND listens.listened_to_end != 1 
+   ORDER BY listens.time_of_listen DESC
+   LIMIT """+limit+";""")
+  else:
+    sql = text("""SELECT listens.youtube_id
+   , listens.time_of_listen
+   , videos.youtube_title
+   , videos.title
+   , videos.release_date
+   , videos.music
+   , artists.artist_name as artist
+   , cities.name
+   , albums.name as album
+   FROM listens
+   JOIN videos ON listens.youtube_id = videos.youtube_id
+   JOIN albums ON videos.album_id = albums.id
+   JOIN artists ON videos.artist_id = artists.id
+   JOIN cities ON artists.city_id = cities.id
+   JOIN saved_vids ON saved_vids.user_id = listens.user_id
+   WHERE listens.user_id = """+str(user_id)+"""
+   AND listens.time_of_listen > '2016-07-05 19:12:18' 
+   AND listens.time_of_listen < '2016-07-10 19:12:18'
+   AND listens.youtube_id != saved_vids.youtube_id
+   AND listens.listened_to_end != 1 
+   ORDER BY listens.time_of_listen DESC
+   LIMIT 3;""")
+
+  results = models.engine.execute(sql)
+  for result in results:
+    print result
+  return result #for now
+
+
+@app.route('/getgenres')
+def getgenres(youtube_id):
+  sql = text("""SELECT 
+genres.name
+,videos.youtube_title
+,videos.youtube_id
+,videos.title
+,artists.artist_name
+FROM videos
+JOIN vids_genres ON videos.youtube_id = vids_genres.youtube_id
+JOIN genres ON vids_genres.genre_id = genres.id
+JOIN artists ON videos.artist_id = artists.id
+WHERE videos.youtube_id ='"""+youtube_id+"';")
+  result = models.engine.execute(sql)
+  return result
+
+
+@app.route('/getsimilarartists')
+def getsimilarartists(youtube_id):
+  sql = text("""SELECT 
+a1.artist_name
+FROM videos
+JOIN similar_artists s1 ON videos.artist_id = s1.artist_id1
+JOIN artists a1 ON s1.artist_id2 = a1.id
+WHERE videos.youtube_id = '"""+youtube_id+"""';""")
+  result1 = models.engine.execute(sql)
+
+  for result in result1:
+    print result
+
+  sql = text("""SELECT 
+a2.artist_name
+FROM videos
+JOIN similar_artists s2 ON videos.artist_id = s2.artist_id2
+JOIN artists a2 ON s2.artist_id1 = a2.id
+WHERE videos.youtube_id = '"""+youtube_id+"""';""")
+  result2 = models.engine.execute(sql)
+
+  for result in result2:
+    print result
+  #result = result1 + result2
+  #result = list(set(result)) #remove redundancies
+  return "success"
 
 @app.route('/login', methods=['GET', 'POST'])
 
@@ -70,6 +172,24 @@ def login():
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+
+
+
+    """
+    {'youtube_id': result[0],
+    'time_of_listen': result[1],
+    'title': result[2],
+    'release_date': result[3],
+    'music': result[4],
+    'artist': result[5],
+    'city_name': result[6],
+    'album': result[7]
+    }
+    """
+
+
+
 
 
 
