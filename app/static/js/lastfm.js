cdae67f54c9f037a371012459ba00587c5ca08f6
elenacaseyroby <elenacaseyroby@gmail.com>
2016-07-28 00:45:22 -0400

    added get genres and get cities (the latter is yet to be matched to artist and stored in db)

--- app/static/js/lastfm.js
+++ app/static/js/lastfm.js
@@ -1,3 +1,10 @@
+// Class to hold Last.fm API track
+function albumInfo(album, trackNumber, year) {
+    this.album = album;
+    this.trackNumber = trackNumber;
+    this.year = year;
+}
+
 function lastFMGetSimilarArtists(artistName, callBack) {
     var similarartiststring = [];
     //console.log("artistName: ", artistName);
@@ -24,10 +31,8 @@ function lastFMGetSimilarArtists(artistName, callBack) {
         }
     });
 }
-//get album and track number and year
-//method=track.getInfo&api_key=YOUR_API_KEY&artist=cher&track=believe&format=json
 
-function lastFMGetGenresByTrack(title, artistName) {
+function lastFMGetGenresByTrack(title, artistName, callBack) {
     var tags = [];
     //var track_num is null;
     
@@ -43,20 +48,91 @@ function lastFMGetGenresByTrack(title, artistName) {
         dataType: "jsonp",
         success: function (data) {
             console.log(data);
-            if(data.track.toptags != null){
+            if(data.track.toptags.tag.length > 0){
+                console.log("data not null");
                 $.each(data.track.toptags.tag, function(index=100, item){
-                    console.log(item.name);
-                    
                     tags.push(item.name);
                 });
             }else{
+                console.log("music!");
                 tags.push('music');
             }
-            //callBack(tags);
-            return tags;
+            console.log("js tags");
+            console.log(tags);
+            callBack(tags);
+            
         }
     });
 }
+//method=artist.getinfo&artist=Cher&api_key=YOUR_API_KEY&format=json
+function lastFMGetCities(artistName) {
+    //var track_num is null;
+    console.log(artistName);
+    results = $.ajax({
+        url: "http://ws.audioscrobbler.com/2.0/",
+        data: "method=artist.getInfo" +
+            "&api_key=175f6031e4788e29a9ff3961219ffc06" +
+            "&artist=" +
+            artistName +
+            "&format=json",
+        dataType: "jsonp",
+        success: function (data) {
+            console.log(data);
+            /*
+            if(data.track.toptags.tag.length > 0){
+                console.log("data not null");
+                $.each(data.track.toptags.tag, function(index=100, item){
+                    tags.push(item.name);
+                });
+            }else{
+                console.log("music!");
+                tags.push('music');
+            }
+            console.log("js tags");
+            console.log(tags);*/
+            //callBack(tags);
+            
+        }
+    });
+}
+
+/*
+function lastFMGetTrackNum(artistName, albumName, trackName) {
+    
+    //var track_num is null;
+    
+    results = $.ajax({
+        url: "http://ws.audioscrobbler.com/2.0/",
+        data: "method=album.getInfo" +
+            "&api_key=175f6031e4788e29a9ff3961219ffc06" +
+            "&artist=" +
+            artistName +
+            "&album=" +
+            albumName +
+            "&format=json",
+        dataType: "jsonp",
+        success: function (data) {
+            var trackNum = null;
+            console.log(data);
+            if(data.track.toptags != null){
+                $.each(data.album.tracks.track, function(index=100, item){
+                    if (item.name == trackName) {
+                        trackNum = index + 1;
+                    }
+                
+                });
+            }
+            //callBack(tags);
+            return trackNum;
+        }
+    });
+}*/
+
+
+//get album and track number and year
+//method=track.getInfo&api_key=YOUR_API_KEY&artist=cher&track=believe&format=json
+
+
 //change to get album by track
 /*
 function lastFMGetGenresByTrack(artistName) {
@@ -87,6 +163,7 @@ function lastFMGetGenresByTrack(artistName) {
 }*/
 
 //if we don't already have it
+/*
 function lastFMGetAlbumYear(title, artistName, callBack) {
 
     var track_name ="";
@@ -104,17 +181,17 @@ function lastFMGetAlbumYear(title, artistName, callBack) {
         dataType: "jsonp",
         success: function (data) {
             //console.log(data);
-            /*
+            
             $.each(data.similarartists.artist, function(index=100, item){
                 console.log(item);
                 albums.push(item.name);
             });
             callBack(albums);
-            */
+            
             //return albums;
         }
     });
-}/*
+}*//*
 function getLastFMData(artistName,Title,Album, callback){
 
 
