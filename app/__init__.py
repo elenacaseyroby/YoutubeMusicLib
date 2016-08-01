f0836e1c22af44ea956898d54527520c75691010
Taylor Burgess <tayloraburgess@gmail.com>
2016-08-01 15:30:41 -0400

    Create LoginManager() object in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 from sqlalchemy.orm import sessionmaker
 from app import models
 #from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config.from_object('config')
 #db = SQLAlchemy(app)
 
