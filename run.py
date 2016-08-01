edddc2418016fcda3970a944a64a2cf2e7919998
Taylor Burgess <tayloraburgess@gmail.com>
2016-07-31 21:19:02 -0400

    Change Documentation.txt

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 #!flask/bin/python
 from app import app
 import os
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port) #imports app var from the app package 
-					#we made and calls its run method to start server.  
-					#since app var is Flask instance, we didn't have to 
-					#write the run method, it came for free.
+if 'HEROKU_CHECK' in os.environ:
+	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT"))) #imports app var from the app package 
+						#we made and calls its run method to start server.  
+						#since app var is Flask instance, we didn't have to 
+						#write the run method, it came for free.
+else:
+	app.run(debug=True)
