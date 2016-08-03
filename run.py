3e8d5cc6a540ab33b01bb77f890936e7065d5f0d
Taylor Burgess <tayloraburgess@gmail.com>
2016-08-03 17:16:39 -0400

    Disable Flask debug in Heroku deployment
    In if statement that checks 'HEROKU_CHECK' in run.py, remove debug=True parameter from app.run.

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from app import app
 import os
 if 'HEROKU_CHECK' in os.environ:
-	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT"))) #imports app var from the app package 
+	app.run(host='0.0.0.0', port=int(os.environ.get("PORT"))) #imports app var from the app package 
 						#we made and calls its run method to start server.  
 						#since app var is Flask instance, we didn't have to 
 						#write the run method, it came for free.
