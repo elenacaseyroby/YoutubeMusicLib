9e9ea59a4160aafa5f0684da0a060006d5c53ea9
elenacaseyroby <elenacaseyroby@gmail.com>
2016-12-06 23:51:59 -0500

    accounts for division by 0

--- app/myfunctions.py
+++ app/myfunctions.py
@@ -32,26 +32,39 @@ def sortnumbers(num_list):
     return sortednum;
 
 def getregressionline(array_of_points):
-  n = 0
-  sumx = 0
-  sumy = 0
-  sumxsquared = 0
-  sumxy = 0
-  for point in array_of_points:
-    if point[0] > 0:
-      n = n + 1
-      sumx = sumx + point[0]
-      sumy = sumy + point[1]
-      sumxsquared = sumxsquared + point[0]*point[0]
-      sumxy = sumxy + point[0]*point[1]
+  if array_of_points[0][0] == 0 and array_of_points[0][1] == 0:
+    regression_line = {'m':0, 'b':0}
+    return regression_line
+  else:
+    n = 0
+    sumx = 0
+    sumy = 0
+    sumxsquared = 0
+    sumxy = 0
+    for point in array_of_points:
+      if point[0] > 0:
+        n = n + 1
+        sumx = sumx + point[0]
+        sumy = sumy + point[1]
+        sumxsquared = sumxsquared + point[0]*point[0]
+        sumxy = sumxy + point[0]*point[1]
+    
+    m_top = float(sumxy - (sumy*(sumx/n)))
+    m_bottom = float(sumxsquared - (sumx*(sumx/n)))
+    if(m_bottom == 0 and m_top == 0):
+      m = 0
+    else:
+      m = float(m_top/m_bottom)
+    b_top = float(sumy - m*sumx)
+    b_bottom = float(n)
+    if(b_bottom == 0 and b_top == 0):
+      b = 0
+    else:
+      b = float(b_top/b_bottom)
 
-  m_top = float(sumxy - (sumy*(sumx/n)))
-  m_bottom = float(sumxsquared - (sumx*(sumx/n)))
-  m = float(m_top/m_bottom)
-  b_top = float(sumy - m*sumx)
-  b_bottom = float(n)
-  b = float(b_top/b_bottom)
+    regression_line = {'m':m, 'b':b}
+    return regression_line
 
-  regression_line = {'m':m, 'b':b}
+    
 
-  return regression_line
+
